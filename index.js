569c5ca4a90147d7e1f67760685cbfe9d1b16b81
7te3ep <7te3ep@gmail.com>
2023-04-12 21:54:57 +0200

    can del

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase , ref , push , onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase , ref , push , onValue , remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL:"https://playground-6e058-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -16,8 +16,17 @@ const front = {
 }
 
 const clearInputField = () => inputField.value = ""
-const addItemToShoppingList = (value) => front.shoppingList.innerHTML += `<li>${value}</li>`
-const clearShoppingList = ()=> {front.shoppingList.innerHTML = ""}
+
+const addItemToShoppingList = (value) => {
+    let newEl = document.createElement('li')
+    newEl.textContent = value[1]
+    front.shoppingList.append(newEl)
+    newEl.addEventListener("click",()=>{
+        remove(ref(database,`shoppingList/${value[0]}`))
+    })
+}
+
+const clearShoppingList = ()=> front.shoppingList.innerHTML = ""
 
 front.addButton.addEventListener("click",(e)=>{
     let inputValue = front.inputField.value
@@ -30,7 +39,7 @@ onValue(shoppingListToDb,(snapshot)=>{
     let itemArray = snapshot.val() != null ? Object.entries(snapshot.val()) : []
     clearShoppingList()
     for (let i = 0; i< itemArray.length; i++){
-        let currentItem = itemArray[i][1]
+        let currentItem = itemArray[i]
         addItemToShoppingList(currentItem)
     }
 })
